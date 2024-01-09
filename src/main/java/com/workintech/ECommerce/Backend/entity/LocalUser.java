4f3162dcb65fc2721e684239213fc700c9f8c47f
egesua <egesu06@gmail.com>
2024-01-09 13:52:40 +0300

    created the localuser

---
+++ src/main/java/com/workintech/ECommerce/Backend/entity/LocalUser.java
@@ -0,0 +1,33 @@
+package com.workintech.ECommerce.Backend.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "local_user")
+public class LocalUser {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @Column(name = "username", nullable = false, unique = true)
+    private String username;
+
+    @Column(name = "password", nullable = false)
+    private String password;
+
+    @Column(name = "email", nullable = false, unique = true)
+    private String email;
+
+    @Column(name = "first_name", nullable = false)
+    private String firstName;
+
+    @Column(name = "last_name", nullable = false)
+    private String lastName;
+}
